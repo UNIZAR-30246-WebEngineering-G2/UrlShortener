c81ef064b2c2c51d8e705922fb259fae6032e2d7
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-11-04 00:06:58 +0100

    Data model

--- team/src/main/java/urlshortener/blackgoku/Application.java
+++ team/src/main/java/urlshortener/blackgoku/Application.java
@@ -1,12 +1,19 @@
 package urlshortener.blackgoku;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import urlshortener.blackgoku.domain.User;
+import urlshortener.blackgoku.repository.UserRepository;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer  implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
@@ -15,6 +22,11 @@ public class Application extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("admin@admin.com","admin"));
 	}
 
 }
---
+++ team/src/main/java/urlshortener/blackgoku/web/UserController.java
@@ -0,0 +1,7 @@
+package urlshortener.blackgoku.web;
+
+/**
+ * Created by Fran Menendez Moya on 3/11/16.
+ */
+public class UserController {
+}
