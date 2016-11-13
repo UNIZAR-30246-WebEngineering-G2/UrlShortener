8dfe73a7ea77c75c1efddcd7a9e8edb8b01363ab
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-11-14 00:55:33 +0100

    Advertisement countdown and asking for link after time is up

---
+++ team/src/main/resources/static/js/publicity.js
@@ -0,0 +1,26 @@
+function startTimer(duration, display) {
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(seconds);
+
+        if (--timer < 0) {
+            $.ajax({
+                type : "GET",
+                url : "/obtainPublicityLink",
+                success : function(msg) {
+                    window.location = msg;
+                }
+            });
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var tenseconds = 10,
+        display = $('#time');
+    startTimer(tenseconds, display);
+});
