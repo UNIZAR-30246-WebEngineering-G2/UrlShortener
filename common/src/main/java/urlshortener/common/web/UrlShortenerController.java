084047afa1047e39d76432e96c6dfe1d4ff5ecca
danirueda <danir94@gmail.com>
2016-11-20 20:23:40 +0100

    Tests passed

--- common/src/main/java/urlshortener/common/web/UrlShortenerController.java
+++ common/src/main/java/urlshortener/common/web/UrlShortenerController.java
@@ -12,11 +12,7 @@ import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -192,19 +188,23 @@ public class UrlShortenerController {
 
 	private Cookie findCookie(String value, HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
-        int i = 0;
-        boolean found= false;
-        while(!found && i<cookies.length){
-            if(cookies[i].getName().equals(value)){
-                found = true;
-            }
-            i++;
-        }
-        if(found){
-            return cookies[i-1];
-        } else {
-            return null;
-        }
+		if(cookies == null){
+			return null;
+		} else {
+			int i = 0;
+			boolean found= false;
+			while(!found && i<cookies.length){
+				if(cookies[i].getName().equals(value)){
+					found = true;
+				}
+				i++;
+			}
+			if(found){
+				return cookies[i-1];
+			} else {
+				return null;
+			}
+		}
     }
 
 	private ShortURL createAndSaveIfValid(String url, String sponsor, String urlPublicity, Integer timePublicity,
